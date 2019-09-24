3ae80ed2e3fdc8695f246f8fd37dfdfa940c0e6a
Thestouges <thestouges@gmail.com>
2019-09-24 13:50:06 -0400

    Added simple stored procedures

---
+++ SQLProcedure.sql
@@ -0,0 +1,29 @@
+﻿ALTER PROCEDURE [dbo].[AddMessage]
+	-- Add the parameters for the stored procedure here
+	@message nvarchar(MAX),
+	@parentID int = null
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+	insert into Messenger (Message,ParentMessageID)
+	values (@message,@parentID)
+END
+Go
+
+ALTER PROCEDURE [dbo].[GetAllMessages]
+	-- Add the parameters for the stored procedure here
+	
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+	Select * from Message
+END
+Go
